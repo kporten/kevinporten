43d3e8fca8358a10f7eb4406fe38e159a9e6fbbf
Kevin Porten <hello@kevinporten.dev>
2021-06-17 22:39:34 +0200

    refactor: add linter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,12 +11,6 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => (
       <title>Kevin Porten | Web Developer</title>
       <meta name="description" content="Welcome to my personal homepage" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
-      />
     </Head>
     <div className="flex items-center justify-center lg:min-h-screen">
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+          />
+          <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
+        </Head>
         <body className="bg-white font-montserrat text-trueGray-700 dark:bg-black dark:text-trueGray-400">
           <Main />
           <NextScript />
